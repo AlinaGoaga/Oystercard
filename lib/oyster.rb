ecd125dcbc0da160399f57897003c02a116f04b2
Alina Goaga <goaga.alina@gmail.com>
2018-12-04 17:01:48 +0000

    Added hash to hold completed journeys

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,14 +1,14 @@
 class Oystercard
   attr_reader :balance
   attr_reader :in_journey
-  attr_reader :entry_station
-  attr_reader :exit_station
+  attr_reader :journeys
 
   MAX_BALANCE = 90
   MIN_FARE = 1
 
   def initialize
     @balance = 0
+    @journeys = {}
     # if we want to do subject.balance in testing we'll need to expose it through an attr_reader
     @in_journey = false
   end
@@ -21,14 +21,13 @@ class Oystercard
   def touch_in(entry_station = 'Aldgate East')
     raise 'Insufficient balance. GET RICH BRO!' if @balance < MIN_FARE
     raise 'User already in journey' if @in_journey == true
-    @entry_station = entry_station
+    @journeys[:entry_station] = entry_station
     @in_journey = true
   end
 
   def touch_out(exit_station = 'Blackfriars')
     deduct
-    @entry_station = nil
-    @exit_station = exit_station
+    @journeys[:exit_station] = exit_station
     @in_journey = false
   end
 
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -3,9 +3,18 @@ require 'oyster'
 RSpec.describe Oystercard do
   let(:entry_station) { double :station }
   let(:exit_station) { double :station }
+  let(:journey) { { entry_station: entry_station, exit_station: exit_station } }
 
   it 'should return a balance of 0 when initialized' do
     expect(subject.balance).to eq 0
+  end
+
+  it 'has an empty list of journeys by default' do
+    expect(subject.journeys).to be_empty
+  end
+
+  it 'when initialized card starts not in journey' do
+    expect(subject.in_journey).to eq false
   end
 
   describe '#top_up' do
@@ -35,19 +44,6 @@ RSpec.describe Oystercard do
     end
   end
 
-  it 'stores the entry station' do
-    subject.top_up(20)
-    subject.touch_in(entry_station)
-    expect(subject.entry_station).to eq entry_station
-  end
-
-  # this is the same as the above, it's just using a string rather than the double
-  # it 'should remember entry station after touch in' do
-  #   subject.top_up(20)
-  #   subject.touch_in('Aldgate East')
-  #   expect(subject.entry_station).to eq 'Aldgate East'
-  # end
-
   describe '#touch_out' do
     it 'should set status to not in journey when the user touches out' do
       subject.top_up(20)
@@ -60,23 +56,12 @@ RSpec.describe Oystercard do
       subject.touch_in
       expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MIN_FARE)
     end
-
-    it 'stores exit station' do
-      subject.top_up(20)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject.exit_station).to eq exit_station
-    end
-
-    it 'should set entry station to nil upon touch out' do
-      subject.top_up(20)
-      subject.touch_in
-      subject.touch_out
-      expect(subject.entry_station).to be nil
-    end
   end
 
-  it 'when initialized card starts not in journey' do
-    expect(subject.in_journey).to eq false
+  it 'registers a completed journey' do
+    subject.top_up(20)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.journeys).to include(journey)
   end
 end
